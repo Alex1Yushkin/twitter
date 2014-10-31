870893e6f129cf4447009fa5933f549fac9b2aac
Alex1Yushkin <a.yushkin@hotmail.com>
2014-10-31 13:27:23 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,19 @@ class SessionsController < ApplicationController
   def new
   end
   
-  def create
+ 
+    
+    def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      flash[:success] = "Добро пожаловать!"
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
-      flash.now[:error] = 'Попробуйте еще раз' # Not quite right!
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
+
   
   def destroy
     sign_out
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,10 +11,7 @@ module SessionsHelper
     !current_user.nil?
   end
   
-  def current_user?(user)
-    user == current_user
-  end
-  
+   
   def current_user=(user)
     @current_user = user
   end
@@ -22,6 +19,10 @@ module SessionsHelper
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
+  end
+    
+  def current_user?(user)
+    user == current_user
   end
   
   def sign_out
@@ -39,4 +40,6 @@ module SessionsHelper
   def store_location
     session[:return_to] = request.url if request.get?
   end
+  
+  
 end
